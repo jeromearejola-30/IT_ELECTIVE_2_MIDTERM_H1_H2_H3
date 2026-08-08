812f128ecd0a34233dc960abfa4ea8d0a913af79
jeromearejola-30 <jeromearejola30@gmail.com>
2026-08-08 16:48:59 +0800

    updated it

--- IT_ELECTIVE_2_MIDTERM_H1_H2_H3/IT_ELECTIVE_2_MIDTERM_H1_H2_H3/Models/Entities/Product.cs
+++ IT_ELECTIVE_2_MIDTERM_H1_H2_H3/IT_ELECTIVE_2_MIDTERM_H1_H2_H3/Models/Entities/Product.cs
@@ -4,8 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
-        public string ImageUrl { get; set; } = string.Empty;
+        public int StockQuantity { get; set; }
     }
 }
